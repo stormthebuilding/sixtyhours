850ec59c22a058c11fb383fcfe20d8d548f42369
Caleb Nguyen <cnguy327@students.bju.edu>
2020-04-11 21:14:53 -0400

    save and load methods

--- src/main/java/Model/World.java
+++ src/main/java/Model/World.java
@@ -1,9 +1,12 @@
 package Model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
+
 
 public class World  {
     //arguments for the world
@@ -73,34 +76,72 @@ public class World  {
         PrintWriter printWriter = new PrintWriter(fileWriter);
         
         for (Object object : objectCollection) {
-         
-            if (object instanceof Enemy) {
-                printWriter.print(((Enemy) object).serialize());
-            }
-            else if (object instanceof HighScore) {
-                printWriter.print(((HighScore) object).serialize());
-            }
-            else if (object instanceof Player) {
-                printWriter.print(((Player) object).serialize());
-            }
-            else if (object instanceof Score) {
-                printWriter.print(((Score) object).serialize());
+            if (object instanceof Player) {
+                printWriter.println("PLAYER");
+                printWriter.println(((Player) object).serialize()+";");
             }
             else if (object instanceof Stronghold) {
-                printWriter.print(((Stronghold) object).serialize());
+                printWriter.println("STRONGHOLD");
+                printWriter.println(((Stronghold) object).serialize()+";");
+            }
+            else if (object instanceof Score) {
+                printWriter.println("SCORE");
+                printWriter.println(((Score) object).serialize()+";");
+            }
+            else if (object instanceof HighScore) {
+                printWriter.println("HIGHSCORE");
+                printWriter.println(((HighScore) object).serialize()+";");
+            }
+            else if (object instanceof Enemy) {
+                printWriter.println("ENEMY");
+                printWriter.println(((Enemy) object).serialize()+";");
             }
             // else if (object instanceof Weapon) {
-            //     printWriter.print((Weapon) object).serialize();
+            //     printWriter.println((Weapon) object).serialize()+";";
             // }
+            if (objectCollection.indexOf(object) == objectCollection.size() -1 ) {
+                printWriter.print("END");
+            }
         }
         printWriter.close();
     }
 
     /**
-     * 
+     * https://stackoverflow.com/questions/5868369/how-can-i-read-a-large-text-file-line-by-line-using-java Used with modifications.
      * @param fileName - The name of the file to extract data from
      */
-    public void load(String fileName) {
+    public void load(String fileName) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("fileName"))) {
+            String line;
+            while (!(line = reader.readLine()).equals("END")) {
+                Object object = null;
+                if (line.equals("PLAYER")) {
+                    object = new Player();
+                    ((Player) object).deserialize(line);
+                }
+                else if (line.equals("STRONGHOLD")) {
+                    object = new Stronghold();
+                    ((Stronghold) object).deserialize(line);
+                }
+                else if (line.equals("SCORE")) {
+                    object = new Score("", 0, DifficultyType.NORMAL);
+                    ((Score) object).deserialize(line);
+                }
+                // else if (line.equals("HIGHSCORE")) {
+                //     object = new HighScore();
+                //     ((HighScore) object).deserialize(line);
+                // }
+                else if (line.equals("ENEMY")) {
+                    object = new Enemy(EnemyType.BASIC);
+                    ((Enemy) object).deserialize(line);
+                }
+                // else if (line.equals("WEAPON")) {
+                //     object = new Weapon();
+                //     ((Weapon) object).deserialize(line);
+                // }
+            }
+        }
 
     }
 }
