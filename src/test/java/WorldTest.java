71b484e9eb410ec10f51b1bbfe316b64ceb032b4
Caleb Nguyen <cnguy327@students.bju.edu>
2020-04-11 23:31:36 -0400

    added WorldTest

---
+++ src/test/java/WorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class WorldTest {
+    @Test
+    public void testSave_SaveToFile_Pass() {
+
+    }
+
+    @Test 
+    public void testLoad_LoadFromFile_Pass() {
+        
+    }
+}
