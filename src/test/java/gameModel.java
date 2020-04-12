b59b75476cd724b8f82258226bcca618db3ea820
Haicheng Wang <hwang129@students.bju.edu>
2020-04-11 23:58:30 -0400

    submission

---
+++ src/test/java/gameModel.java
@@ -0,0 +1,18 @@
+import Model.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class gameModel {
+    @Test
+    public void enemyAttack() {
+
+    }
+    @Test
+    public void playerAttack() {
+
+    }
+    @Test
+    public void stongHoldRepariAttack() {
+
+    }
+}
