b733b4ba260303e982ca0cdd765c9bc2a4b69460
Caleb Nguyen <cnguy327@students.bju.edu>
2020-04-11 20:01:22 -0400

    save method

--- src/main/java/Model/World.java
+++ src/main/java/Model/World.java
@@ -1,12 +1,19 @@
 package Model;
 
-public class World {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class World  {
     //arguments for the world
     private String difficulty;
     private String userName;
     private int id;
 
     private static int nextId;
+
+    private ArrayList<Object> objectCollection = new ArrayList<Object>();
 
     //Store methods
 
@@ -56,5 +63,44 @@ public class World {
         this.id = id;
     }
 
+    /**
+     * 
+     * @param fileName - The name of the file to save data to
+     */
+    public void save(String fileName) throws IOException {
 
+        FileWriter fileWriter = new FileWriter("filePath", true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        
+        for (Object object : objectCollection) {
+         
+            if (object instanceof Enemy) {
+                printWriter.print(((Enemy) object).serialize());
+            }
+            else if (object instanceof HighScore) {
+                printWriter.print(((HighScore) object).serialize());
+            }
+            else if (object instanceof Player) {
+                printWriter.print(((Player) object).serialize());
+            }
+            else if (object instanceof Score) {
+                printWriter.print(((Score) object).serialize());
+            }
+            else if (object instanceof Stronghold) {
+                printWriter.print(((Stronghold) object).serialize());
+            }
+            // else if (object instanceof Weapon) {
+            //     printWriter.print((Weapon) object).serialize();
+            // }
+        }
+        printWriter.close();
+    }
+
+    /**
+     * 
+     * @param fileName - The name of the file to extract data from
+     */
+    public void load(String fileName) {
+
+    }
 }
