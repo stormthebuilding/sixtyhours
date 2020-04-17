61ee59946562d04ee9a4df556640bb494a4e1bb5
khans <khans685@students.bju.edu>
2020-04-17 14:43:12 -0400

    added map view to gamewindow

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,16 +1,23 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 
 
 public class MainWindow {
 
     @FXML
-    void onGreetClicked(ActionEvent event) {
-        var alert = new Alert(AlertType.INFORMATION, "Hello, world!");
-        alert.setHeaderText(null);
-        alert.show();
+    void onStartClicked(ActionEvent event) throws IOException {
+        var loader = new FXMLLoader(getClass().getResource("GameWindow.fxml"));
+        var scene = new Scene(loader.load());
 
+        var stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
