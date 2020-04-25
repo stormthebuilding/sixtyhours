e261fbd2502f9826699e42990b6e2b5b205e4f7c
khans <khans685@students.bju.edu>
2020-04-25 03:11:31 -0400

    finished enemies

--- src/main/java/Model/Enemies/Heavy.java
+++ src/main/java/Model/Enemies/Heavy.java
@@ -6,6 +6,9 @@ import Model.EnemyType;
 public class Heavy extends Enemy {
     public Heavy(EnemyType type) {
         super(type);
+        speed = 3;
+        health = 35;
+        damage = 1.5;
     }
 
 }
